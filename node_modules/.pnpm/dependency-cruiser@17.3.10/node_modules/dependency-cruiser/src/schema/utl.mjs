abdc548456c581aed79bbf15f207fc725a1505d5
Jose Costa <j.costa.3394@gmail.com>
2026-04-23 22:28:19 +0200

    chore(gitignore): add .gitignore and untrack node_modules

--- node_modules/.pnpm/dependency-cruiser@17.3.10/node_modules/dependency-cruiser/src/schema/utl.mjs
+++
@@ -1,6 +0,0 @@
-export function validateErrorsToString(pErrors) {
-	/* c8 ignore next 1 */
-	return (pErrors || [])
-		.map((pError) => `data${pError.instancePath} ${pError.message}`)
-		.join(", ");
-}
